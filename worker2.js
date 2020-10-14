200d72d752a36898c028c4ad12207696c5a98441
JinShan.Zhou <JinShan.Zhou@zoom.us>
2020-10-14 18:55:10 +0800

    test

---
+++ worker2.js
@@ -0,0 +1,35 @@
+
+var g_sharbuffer = null;
+var g_encodeSharedBufferState = null;
+var g_encodeSharedBufferData = null;
+
+var g_decodeSharedBufferState = null;
+var g_decodeSharedBufferData = null;
+
+
+function Read_Data() {
+    let now = 0;
+    for (;;) {
+        now = new Date().getTime();
+        Atomics.wait(g_decodeSharedBufferSig, 0, 0) ;
+        let sig = Atomics.load(g_decodeSharedBufferSig, 0);
+        console.log(new Date().getTime() - now);
+
+        Atomics.store(g_decodeSharedBufferSig, 0, 0);
+    }
+}
+
+self.addEventListener('message', function (e) {
+    var message = e.data;
+
+    switch(message.command) {
+        case "init" : {
+            g_sharbuffer = message.data;
+            g_decodeSharedBufferSig = new Int32Array(g_sharbuffer.outputSig);
+            g_decodeSharedBufferSig[0] = 0;
+            setInterval(Read_Data, 5);
+            break;
+        }
+    }
+});
+
---
+++ worklet_test3.js
@@ -0,0 +1,41 @@
+var g_sharbuffer = null;
+var g_encodeSharedBufferState = null;
+var g_encodeSharedBufferData = null;
+
+var g_decodeSharedBufferState = null;
+var g_decodeSharedBufferData = null;
+var g_decodeSharedBufferSig = null;
+
+class myworklet extends AudioWorkletProcessor {
+    constructor() {
+        // The super constructor call is required.
+        super();
+        this.port.onmessage = this.handleMessage.bind(this);
+    }
+
+    handleMessage(event) {
+        var data = event.data;
+        switch (data.status) {
+            case "sharedBuffer":{
+                const sharebuf = data.data;
+                g_sharbuffer = sharebuf;
+                g_decodeSharedBufferSig = new Int32Array(g_sharbuffer.outputSig);
+                console.log("AudioWorkletProcessor", g_sharbuffer);
+                break;
+            }
+        }
+    }
+
+
+
+    process(inputs, outputs, parameters) {
+        if (inputs.length === 0 || inputs[0].length === 0) return true;
+        if (!g_sharbuffer) return true;
+
+        Atomics.store(g_decodeSharedBufferSig, 0, 1);
+        Atomics.notify(g_decodeSharedBufferSig, 0, 1);
+        return true;
+    }
+}
+
+registerProcessor('myworklet', myworklet);
